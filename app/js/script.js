944b5bfefdac76e875a5b7efbd08f72c30187e40
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-01-25 15:40:30 +0700

    Completed following episode 3

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,23 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElements = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", () => {
   if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElements.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
+    body.classList.add("noscroll");
     header.classList.add("open");
-    overlay.classList.add("fade-in");
-    overlay.classList.remove("fade-out");
+    fadeElements.forEach((element) => {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
   }
 });
